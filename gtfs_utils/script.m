a2a0da63cfc593f5376a8cc6491a380a7bb21208
Vincent Maison <pro@vincent.maison>
2018-03-21 15:48:50 +0100

    Added a plot of stations in script, with gps info

--- gtfs_utils/script.m
+++ gtfs_utils/script.m
@@ -26,7 +26,8 @@ gen_c = readtable(file_connections, ...
 from_to_array = table2array(gen_c(:,1:2));
 stops_array = [double(table2array(db.stops(:,{'stop_id'}))), ...
     table2array(db.stops(:,{'stop_lat', 'stop_lon'}))];
-rand_scale = 6E-4;
+%rand_scale = 6E-4;
+rand_scale = 0;
 stops_array(:,2:3) = stops_array(:,2:3)+rand(size(stops_array(:,2:3)))*rand_scale;
 
 uniq_from_to = unique(from_to_array, 'rows');
@@ -36,26 +37,14 @@ uniq_from_to = unique(from_to_array, 'rows');
 
 %% Drawing graph, showing some interesting nodes
 fh = figure;
-symbol = '.';
+symbol = 'o';
 for ind = 1:size(uniq_from_to,1)
-   plot([stops_array(loc1(ind),3), stops_array(loc2(ind),3)], ...
-     [stops_array(loc1(ind),2), stops_array(loc2(ind),2)], [symbol, 'k'], ...
-     'MarkerSize',5);
-  hold on;
+  plot([stops_array(loc1(ind),3), stops_array(loc2(ind),3)], ...
+     [stops_array(loc1(ind),2), stops_array(loc2(ind),2)], [symbol, 'r'], ...
+     'MarkerSize',0.1);
   patch('XData', [stops_array(loc1(ind),3), stops_array(loc2(ind),3)], ...
-    'YData', [stops_array(loc1(ind),2), stops_array(loc2(ind),2)], 'LineWidth', 0.1);
+    'YData', [stops_array(loc1(ind),2), stops_array(loc2(ind),2)], 'LineWidth', 0.3);
+  hold on;
 end
 axis equal;
 
-% 2117 et 2404
-db.stops([465,735,6191,11605,389,300,375],:)
-% Fixed with a good hour choice (18h to 18h05)
-plot(pts(465,2), pts(465,1), [symbol, 'b'])
-plot(pts(6191,2), pts(6191,1), [symbol, 'b'])
-plot(pts(735,2), pts(735,1), [symbol, 'b'])
-% Fixed : Line 4 : clignancourt simplon marcadet chateau rouge, connections?
-plot(pts(11605,2), pts(11605,1), [symbol, 'b'])
-plot(pts(389,2), pts(389,1), [symbol, 'b'])
-% Fixed : Line 6 : dupleix to cambronne?
-plot(pts(300,2), pts(300,1), [symbol, 'b'])
-plot(pts(375,2), pts(375,1), [symbol, 'b'])
