3f6164ddcb99cfe4bdd6d1d9c5b147a43a47d612
Vincent Maison <pro@vincent.maison>
2018-03-21 15:48:10 +0100

    Little tweaks
    - Added verbose and warnings
    - Change non-retrocompatible way to make a table
    - Added a duration calculation for pre-R2018a
    - Minor tweaks

--- gtfs_utils/make_gtfs_files.m
+++ gtfs_utils/make_gtfs_files.m
@@ -1,21 +1,35 @@
 function [] = make_gtfs_files(db, fn1, fn2, cmode);
 % maybe add a gps coord toogle?
+verbose = 0;
+
+msg1 = ['BEWARE, to work with the RATP database, you need at least ', ...
+    '3,3Go of free memory (including db, which is the RATP ', ...
+    'database, and should be ~1,2Go)'];
+msg2 = ['This function is tested on MATLAB R2017a and optimized ', ...
+    'for MATLAB R2018a.'];
+warning(msg1);
+warning(msg2);
 
 % do something nicer with this, like put an argument keeping_lines:
 % if it's a string, if it's metro, rer, bus, keep the good lines
 % if its a cell array keep the lines in the variable
 keeping_lines = [{'1'}; {'2'}; {'3'}; {'4'}; {'5'}; {'6'}; {'7'}; {'8'}; ...
-{'9'}; {'10'}; {'11'}; {'12'}; {'13'}; {'14'}; {'3B'}; {'7B'}]
+{'9'}; {'10'}; {'11'}; {'12'}; {'13'}; {'14'}; {'3B'}; {'7B'}];
 
 % seconds added for each duration of connections to avoid some zeros
 offset_connect = 30;
+
+if verbose
+  keeping_lines'
+  offset_connect
+end
+
 
 %% All connections from trips
 t_start_t_connections = tic;
 
 % there is at least two adjacent lines with the same trip, let's
 % do connections that way
-nb_connect = size(db.stop_times,1)-1;
 connect_trips = [db.stop_times{1:end-1,1}, db.stop_times{2:end,1}];
 good_trips = find(connect_trips(:,1)==connect_trips(:,2));
 connect_trips = connect_trips(good_trips,:);
@@ -26,7 +40,11 @@ db.stop_times = [];
 
 % transform times into seconds is expansive, so we will do it after
 % the last reduction of this matrix
-t_generation_connections = toc(t_start_t_connections)
+t_generation_connections = toc(t_start_t_connections);
+if verbose
+  t_generation_connections
+end
+
 
 %% All stops from connections
 t_start_t_stops = tic;
@@ -58,11 +76,13 @@ if cmode == 0
 else
   % remove all_stop commas
   all_stops(:,2:end) = strrep(all_stops(:,2:end),',', '');
-
   all_stops = all_stops(:,[3,1,2,4,5]);
 end
+t_generation_stops = toc(t_start_t_stops);
+if verbose
+  t_generation_stops
+end
 
-t_generation_stops = toc(t_start_t_stops)
 
 %% Same stops as instant connections NOTE(vincent): not fully tested
 t_start_t_same_stops = tic;
@@ -77,11 +97,10 @@ all_same_stops = zeros(100*size(db.stops,1), 3, 'uint32');
 
 cpt = 0;
 for ind = 1:(numel(idx_sort) - 1)
-  curr_id = stops_id(idx_sort(ind));
+  reps = stops_id(idx_sort(ind));
   next_id = stops_id(idx_sort(ind+1));
   is_same = stops_id(idx_sort(sorted_unique(ind))) == ...
     stops_id(idx_sort(sorted_unique(ind+1)));
-  reps = [curr_id];
   while is_same
     if ind == (numel(idx_sort) - 1)
       break;
@@ -89,7 +108,6 @@ for ind = 1:(numel(idx_sort) - 1)
       ind = ind + 1;
     end
     reps = [reps, next_id];
-    curr_id = stops_id(idx_sort(ind));
     next_id = stops_id(idx_sort(ind+1));
     is_same = stops_id(idx_sort(sorted_unique(ind))) == ...
       stops_id(idx_sort(sorted_unique(ind+1)));
@@ -103,19 +121,27 @@ for ind = 1:(numel(idx_sort) - 1)
 end
 all_same_stops = all_same_stops(1:cpt,:);
 all_same_stops = unique(all_same_stops, 'rows');
-t_generation_same_stops = toc(t_start_t_same_stops)
-
+t_generation_same_stops = toc(t_start_t_same_stops);
+if verbose
+  t_generation_same_stops
+end
 
 %% Transfers
 t_start_t_transfers = tic;
 all_transfers = [uint32(table2array(db.transfers(:,[1, 2, 4])));
     uint32(table2array(db.transfers(:,[2, 1, 4])))];
 all_transfers = unique(all_transfers, 'rows');
-t_generation_transfers = toc(t_start_t_transfers)
+t_generation_transfers = toc(t_start_t_transfers);
+if verbose
+  t_generation_transfers
+end
 
 
 %% Preparing tables, and removing unwanted lines
 t_start_remove_lines = tic;
+if verbose
+  fprintf('Removing unwanted lines, this takes time...\n')
+end
 
 % We want to export a table of connections,  but the times for each connection
 % isn't computed yet. So we separately compute the good connections and add the
@@ -124,7 +150,6 @@ full_connect = [all_connections; all_same_stops(:,1:2); all_transfers(:,1:2)];
 
 times_stop_tran = [all_same_stops(:,3); all_transfers(:,3)];
 n_t_connect = size(times,1);
-n_t_stop_tran = size(times_stop_tran,1);
 
 % Removing stations and connections of unwanted lines
 removing_stops = ~ismember(all_stops(:,3), keeping_lines);
@@ -138,7 +163,6 @@ full_connect(removing_c,:) = [];
 times_stop_tran(removing_c((n_t_connect+1):end),:) = [];
 times(removing_c(1:n_t_connect),:) = [];
 n_t_connect = size(times,1);
-n_t_stop_tran = size(times_stop_tran,1);
 
 % static discard of unwanted connections :
 % Simplon 1668 2478 - Chateau Rouge 2033 2152
@@ -161,20 +185,33 @@ connections_to_remove = ...
 full_connect(connections_to_remove,:) = [];
 times_stop_tran(connections_to_remove((n_t_connect+1):end),:) = [];
 times(connections_to_remove(1:n_t_connect),:) = [];
+t_remove_lines = toc(t_start_remove_lines);
+if verbose
+  t_remove_lines
+end
 
-t_remove_lines = toc(t_start_remove_lines)
 
 %% Now we can compute the seconds between two times (in a string format)
 t_start_duration_computation = tic;
+
 if version('-date') > datetime(2017,10,31)
   % optimized method for MATLAB R2018a
   times = seconds(duration(times));
 else
-  error('not implemented yet')
+  if verbose
+    fprintf('Entering non-optimized duration calculation, this can take a while...\n')
+  end
+  tmp = datetime(times,'InputFormat','HH:mm:ss');
+  times = hour(tmp)*3600+minute(tmp)*60+second(tmp);
 end
+
 timediff = times(:,2) - times(:,1) + offset_connect;
 times_stop_tran = [timediff;times_stop_tran];
-t_duration_computation = toc(t_start_duration_computation)
+t_duration_computation = toc(t_start_duration_computation);
+if verbose
+  t_duration_computation
+end
+
 
 %% Removing courses out of a specific timeframe, and averaging course duration
 t_start_remove_timeset_and_averaging = tic;
@@ -188,12 +225,15 @@ idx_times_to_merge = accumarray(iA, (1:numel(iA)).', [], @(r){sort(r)});
 
 % connection time by averaging connection time of all differents trips
 new_t_connect = [array2table(full_connect(iB,:)), ...
-  table('Size', size(full_connect(iB,1)), 'VariableTypes', {'double'}, ...
-  'VariableNames', {'transfer_time'})];
+  array2table(zeros(size(full_connect(iB,1))), 'VariableNames', {'transfer_time'});];
 for ind = 1:numel(idx_times_to_merge)
   new_t_connect{ind,3} = round(mean(times_stop_tran(idx_times_to_merge{ind})));
 end
-t_remove_timeset_and_averaging = toc(t_start_remove_timeset_and_averaging)
+t_remove_timeset_and_averaging = toc(t_start_remove_timeset_and_averaging);
+if verbose
+  t_remove_timeset_and_averaging
+end
+
 
 %% Export files
 t_start_t_export_data = tic;
@@ -211,8 +251,11 @@ table_stops.Properties.VariableNames = {'string_name_station', ...
 % table_connections = [table_connections, connection_date)];
 % table_connections.Properties.VariableNames = {'uint32_from_stop_id', ...
 %   'uint32_to_stop_id', 'uint32_min_transfer_time', 'string_connection_date'};
+t_export_data = toc(t_start_t_export_data);
+if verbose
+  t_export_data
+end
 
-t_export_data = toc(t_start_t_export_data)
 
 %% Write files
 t_start_t_csv_write = tic;
@@ -220,4 +263,7 @@ delete(fn1)
 delete(fn2)
 writetable(table_stops, fn1)
 writetable(table_connections, fn2)
-t_generation_csv_write = toc(t_start_t_csv_write)
+t_generation_csv_write = toc(t_start_t_csv_write);
+if verbose
+  t_generation_csv_write
+end
